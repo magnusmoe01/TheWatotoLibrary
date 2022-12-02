b802c4ca23deb5cf7c54ad254e12ff76662de44d
magnusmoe01 <96432497+magnusmoe01@users.noreply.github.com>
2022-12-02 11:39:46 +0100

    Add files via upload

---
+++ js/gtag.js
@@ -0,0 +1,7 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-WTX3771TKG"></script>
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-WTX3771TKG');
