50e4e69e65c9208fbfe979a7a55506797397a66a
magnusmoe01 <96432497+magnusmoe01@users.noreply.github.com>
2022-12-03 21:15:03 +0100

    Delete gtag.js

--- js/gtag.js
+++
@@ -1,7 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-WTX3771TKG"></script>
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-WTX3771TKG');
